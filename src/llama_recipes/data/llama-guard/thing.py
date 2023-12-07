b0855076c43670c7e2402cba98cca0c908f32ca9
Michael Tontchev <michael.tontchev@yahoo.com>
2023-12-07 07:10:55 +0000

    more changes

--- src/llama_recipes/data/llama-guard/thing.py
+++ src/llama_recipes/data/llama-guard/thing.py
@@ -6,127 +6,116 @@ from typing import Dict, List, Literal, Optional, Sequence
 
 
 @dataclass
-class Guideline:
+class Category:
     name: str
     description: str
-    idx: int
 
 
 @dataclass
 class Guidelines:
-    name: str
-    guidelines: Sequence[Guideline]
+    category: Sequence[Category]
+    category_code_prefix: str = "C"
 
 
-# Explanations in the assessments can go before the decision ("PRE"), or after the decision ("POST")
-class ExplanationPosition(Enum):
-    PRE = 0
-    POST = 1
+class DecisionExplanationPosition(Enum):
+    BEFORE_DECISION = 0
+    AFTER_DECISION = 1
 
 
 @dataclass
-class PromptTemplate:
-    name: str
+class LlamaGuardPromptTemplate:
     instructions: str
-    # Flag that indicates whether a prompt template uses the descriptions from the GLs
-    should_include_guideline_descriptions: bool
-    should_shuffle_guideline_codes_in_each_eval_row: bool = True
-    # Flag that indicates whether a prompt template requires fine labels (GL categories) to be printed in assessment
-    requires_fine_labels: bool
+    should_include_category_descriptions: bool
+    should_shuffle_category_codes_in_each_training_example: bool = True
 
 
 @dataclass
 class DatagenArgs:
     guidelines: Guidelines
-    llama_guard_prompt_template: PromptTemplate
-    eval_type: str = "safety_response"
-    guideline_code_prefix: str = "C"
+    llama_guard_prompt_template: LlamaGuardPromptTemplate
     probability_to_augment_with_good_rows_with_empty_generation: float = 0
-    should_augment_with_rows_with_dropped_nonviolated_prompt_guidelines: bool = True
-    should_augment_with_rows_with_dropped_violated_and_nonviolated_prompt_guidelines: bool = (
+    should_augment_with_rows_with_dropped_nonviolated_prompt_categories: bool = True
+    should_augment_with_rows_with_dropped_violated_and_nonviolated_prompt_categories: bool = (
         False
     )
     coarse_label_instruction_prob: float = 0.0
     random_seed: int = 42
+    should_generations_include_violation_codes: bool
     # If present, specifies whether the explanation is shown before or after the decicion in the llama guard generation.
-    explanation_position_in_llama_guard_generation: Optional[ExplanationPosition]
+    explanation_position_in_llama_guard_generation: Optional[DecisionExplanationPosition]
 
 
 @dataclass
-class EvalRow:
+class TrainingExample:
     prompt: str
     generation: str
-    violated_guideline_codes: list[str]
-    label: Literal["safe", "bad"]
+    violated_categorie_codes: list[str]
+    label: Literal["safe", "unsafe"]
     explanation: str
 
 
 # TODO @nocommit HANDLE N/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
 
-def eval_rows_to_sft_rows(
-    eval_rows: Sequence[EvalRow],
+def create_formatted_finetuning_examples(
+    training_examples: Sequence[TrainingExample],
     args: DatagenArgs,
-) -> list[dict[str, str]]:
+) -> list[str]:
     random.seed(args.random_seed)
 
-    indices_of_all_guidelines = range(len(args.guidelines.guidelines))
+    indices_of_all_categories = range(len(args.guidelines.categories))
 
     to_return = []
 
-    for eval_row in eval_rows:
+    for training_example in training_examples:
         to_return.append(
-            make_sft_row(
-                eval_row,
+            create_formatted_finetuning_example(
+                training_example,
                 args,
-                guideline_indeces_to_include_in_llama_guard_prompt=list(
-                    indices_of_all_guidelines
+                category_indeces_to_include_in_llama_guard_prompt=list(
+                    indices_of_all_categories
                 ),
             )
         )
 
         maybe_augment_with_good_row_with_empty_generation(
-            eval_row, to_return, indices_of_all_guidelines, args
+            training_example, to_return, indices_of_all_categories, args
         )
 
-        maybe_augment_with_rows_with_dropped_prompt_guidelines(
-            eval_row, to_return, indices_of_all_guidelines, args
+        maybe_augment_with_rows_with_dropped_prompt_categories(
+            training_example, to_return, indices_of_all_categories, args
         )
 
-    num_empty = to_return.count(None)
-
-    to_return = [row for row in to_return if row is not None]
-
-    print(f"Successfully created {len(to_return)} rows (discarded: {num_empty}).")
+    print(f"Successfully created {len(to_return)} formatted finetuning examples.")
 
     return to_return
 
 
-def make_sft_row(
-    eval_row: EvalRow,
+def create_formatted_finetuning_example(
+    training_example: TrainingExample,
     args: DatagenArgs,
-    guideline_indeces_to_include_in_llama_guard_prompt: List[int],
+    category_indeces_to_include_in_llama_guard_prompt: List[int],
 ):
-    if args.should_shuffle_guideline_codes_in_each_eval_row:
-        random.shuffle(guideline_indeces_to_include_in_llama_guard_prompt)
+    if args.should_shuffle_category_codes_in_each_training_example:
+        random.shuffle(category_indeces_to_include_in_llama_guard_prompt)
     else:
-        guideline_indeces_to_include_in_llama_guard_prompt = sorted(
-            guideline_indeces_to_include_in_llama_guard_prompt
+        category_indeces_to_include_in_llama_guard_prompt = sorted(
+            category_indeces_to_include_in_llama_guard_prompt
         )
 
-    conversation = {"human": eval_row.prompt}
+    conversation = {"human": training_example.prompt}
 
-    if eval_row.generation != "N/A":
-        conversation["chatbot"] = eval_row.generation
+    if training_example.generation != "N/A":
+        conversation["chatbot"] = training_example.generation
 
     llama_guard_prompt = create_llama_guard_prompt(
         conversation,
-        guideline_indeces_to_include_in_llama_guard_prompt,
+        category_indeces_to_include_in_llama_guard_prompt,
         args,
     )
 
     llama_guard_generation = create_llama_guard_generation(
-        eval_row, args, guideline_indeces_to_include_in_llama_guard_prompt
+        training_example, args, category_indeces_to_include_in_llama_guard_prompt
     )
 
     return f"{llama_guard_prompt} {llama_guard_generation}"
@@ -134,21 +123,21 @@ def make_sft_row(
 
 def create_llama_guard_prompt(
     conversation: Dict[str, str],
-    guideline_indices_to_include: List[int],
+    category_indices_to_include: List[int],
     args: DatagenArgs,
 ) -> str:
     full_guidelines_text = ""
 
     for (
-        rewritten_guideline_index_for_current_prompt,
-        original_guideline_index,
-    ) in enumerate(guideline_indices_to_include):
-        guideline = args.guidelines.guidelines[original_guideline_index]
+        rewritten_category_index_for_current_prompt,
+        original_category_index,
+    ) in enumerate(category_indices_to_include):
+        category = args.guidelines.categories[original_category_index]
 
-        full_guidelines_text += f"\n{args.guideline_code_prefix}{rewritten_guideline_index_for_current_prompt + 1}: {guideline.name}. "
+        full_guidelines_text += f"\n{args.guidelines.category_code_prefix}{rewritten_category_index_for_current_prompt + 1}: {category.name}. "
 
-        if args.llama_guard_prompt_template.should_include_guideline_descriptions:
-            full_guidelines_text += f"\n{guideline.description}"
+        if args.llama_guard_prompt_template.should_include_category_descriptions:
+            full_guidelines_text += f"\n{category.description}"
 
     return args.llama_guard_prompt_template.instructions.format_map(
         {
@@ -168,175 +157,175 @@ def serialize_conversation(conversation: Dict[str, str]) -> str:
 
 
 def create_llama_guard_generation(
-    eval_row: EvalRow,
+    training_example: TrainingExample,
     args: DatagenArgs,
-    guideline_indices_to_include_in_llama_guard_prompt: List[int],
+    category_indices_to_include_in_llama_guard_prompt: List[int],
 ) -> str:
-    to_return = eval_row.label
+    to_return = training_example.label
 
-    if eval_row.label == "bad" and args.prompt_template.requires_fine_labels:
-        original_violated_guideline_indices = set(
-            convert_guideline_codes_to_indices(
-                eval_row.violated_guideline_codes, args.guideline_code_prefix
+    if training_example.label == "unsafe" and args.prompt_template.requires_fine_labels:
+        original_violated_category_indices = set(
+            convert_category_codes_to_indices(
+                training_example.violated_category_codes, args.guidelines.category_code_prefix
             )
         )
 
-        map_of_original_guideline_indices_to_rewritten_guideline_codes = get_map_of_original_guideline_indices_to_rewritten_guideline_codes(args, guideline_indices_to_include_in_llama_guard_prompt)
+        map_of_original_category_indices_to_rewritten_category_codes = get_map_of_original_category_indices_to_rewritten_category_codes(args, category_indices_to_include_in_llama_guard_prompt)
 
-        rewritten_violated_guideline_codes = [map_of_original_guideline_indices_to_rewritten_guideline_codes[original_violated_index] for original_violated_index in original_violated_guideline_indices]
+        rewritten_violated_category_codes = [map_of_original_category_indices_to_rewritten_category_codes[original_violated_index] for original_violated_index in original_violated_category_indices]
 
         to_return += "\n"
-        to_return += ','.join(rewritten_violated_guideline_codes)
+        to_return += ','.join(rewritten_violated_category_codes)
 
     if args.explanation_position_in_llama_guard_generation is not None:
-        if args.explanation_position_in_llama_guard_generation == ExplanationPosition.PRE:
-            to_return = f"Explanation: {eval_row.explanation}\n{to_return}"
-        elif args.explanation_position_in_llama_guard_generation == ExplanationPosition.POST:
-            to_return = f"{to_return}\nExplanation: {eval_row[args.explanation_key]}"
+        if args.explanation_position_in_llama_guard_generation == DecisionExplanationPosition.BEFORE_DECISION:
+            to_return = f"Explanation: {training_example.explanation}\n{to_return}"
+        elif args.explanation_position_in_llama_guard_generation == DecisionExplanationPosition.AFTER_DECISION:
+            to_return = f"{to_return}\nExplanation: {training_example[args.explanation_key]}"
 
     return to_return
 
-def get_map_of_original_guideline_indices_to_rewritten_guideline_codes(
+def get_map_of_original_category_indices_to_rewritten_category_codes(
     args: DatagenArgs,
-    guideline_indices_to_include_in_llama_guard_prompt: List[int]
+    category_indices_to_include_in_llama_guard_prompt: List[int]
 ):
     return {
-        original_guideline_index: args.guideline_code_prefix + str(rewritten_guideline_index + 1)
-        for rewritten_guideline_index, original_guideline_index in enumerate(
-            guideline_indices_to_include_in_llama_guard_prompt
+        original_category_index: args.guidelines.category_code_prefix + str(rewritten_category_index + 1)
+        for rewritten_category_index, original_category_index in enumerate(
+            category_indices_to_include_in_llama_guard_prompt
         )
     }
 
 def maybe_augment_with_good_row_with_empty_generation(
-    eval_row: EvalRow,
+    training_example: TrainingExample,
     sft_rows_to_build: list[dict[str, str]],
-    indices_of_all_guidelines: range,
+    indices_of_all_categories: range,
     args: DatagenArgs,
 ) -> None:
     if (
         random.random()
         < args.probability_to_augment_with_good_rows_with_empty_generation
     ):
-        eval_row_copy = copy.deepcopy(eval_row)
-        eval_row_copy.generation = ""
-        eval_row_copy.label = "safe"
-        eval_row_copy.violated_guideline_codes = []
+        training_example_copy = copy.deepcopy(training_example)
+        training_example_copy.generation = ""
+        training_example_copy.label = "safe"
+        training_example_copy.violated_category_codes = []
 
         sft_rows_to_build.append(
-            make_sft_row(
-                eval_row_copy,
+            create_formatted_finetuning_example(
+                training_example_copy,
                 args,
-                guideline_indeces_to_include_in_llama_guard_prompt=list(
-                    indices_of_all_guidelines
+                category_indeces_to_include_in_llama_guard_prompt=list(
+                    indices_of_all_categories
                 ),
             )
         )
 
 
-def maybe_augment_with_rows_with_dropped_prompt_guidelines(
-    eval_row: EvalRow,
+def maybe_augment_with_rows_with_dropped_prompt_categories(
+    training_example: TrainingExample,
     sft_rows_to_build: list[dict[str, str]],
-    indices_of_all_guidelines: range,
+    indices_of_all_categories: range,
     args: DatagenArgs,
 ) -> None:
-    violated_guideline_indices = convert_guideline_codes_to_indices(
-        eval_row.violated_guideline_codes, args.guideline_code_prefix
+    violated_category_indices = convert_category_codes_to_indices(
+        training_example.violated_category_codes, args.guidelines.category_code_prefix
     )
 
-    nonviolated_guideline_indices = list(
-        set(indices_of_all_guidelines) - set(violated_guideline_indices)
+    nonviolated_category_indices = list(
+        set(indices_of_all_categories) - set(violated_category_indices)
     )
 
-    maybe_augment_with_row_with_dropped_nonviolated_prompt_guidelines(
-        eval_row,
+    maybe_augment_with_row_with_dropped_nonviolated_prompt_categories(
+        training_example,
         sft_rows_to_build,
-        indices_of_all_guidelines,
-        nonviolated_guideline_indices,
+        indices_of_all_categories,
+        nonviolated_category_indices,
         args,
     )
 
-    maybe_augment_with_row_with_dropped_violated_and_nonviolated_prompt_guidelines(
-        eval_row,
+    maybe_augment_with_row_with_dropped_violated_and_nonviolated_prompt_categories(
+        training_example,
         sft_rows_to_build,
-        indices_of_all_guidelines,
-        violated_guideline_indices,
-        nonviolated_guideline_indices,
+        indices_of_all_categories,
+        violated_category_indices,
+        nonviolated_category_indices,
         args,
     )
 
 
-def convert_guideline_codes_to_indices(codes: list[str], code_prefix: str) -> list[int]:
+def convert_category_codes_to_indices(codes: list[str], code_prefix: str) -> list[int]:
     # The string indexing of GLs is 1-based, so we convert to 0-based here for consistency with python indexing
     return [int(code.lstrip(code_prefix)) - 1 for code in codes]
 
 
-def maybe_augment_with_row_with_dropped_nonviolated_prompt_guidelines(
-    eval_row: EvalRow,
+def maybe_augment_with_row_with_dropped_nonviolated_prompt_categories(
+    training_example: TrainingExample,
     sft_rows_to_build: list[dict[str, str]],
-    indices_of_all_guidelines: range,
-    nonviolated_guideline_indices: list[int],
+    indices_of_all_categories: range,
+    nonviolated_category_indices: list[int],
     args: DatagenArgs,
 ) -> None:
-    if not args.should_augment_with_rows_with_dropped_nonviolated_prompt_guidelines:
+    if not args.should_augment_with_rows_with_dropped_nonviolated_prompt_categories:
         pass
 
-    number_of_guidelines_to_drop = random.randint(0, len(nonviolated_guideline_indices))
+    number_of_categories_to_drop = random.randint(0, len(nonviolated_category_indices))
 
-    if number_of_guidelines_to_drop == len(indices_of_all_guidelines):
-        number_of_guidelines_to_drop -= 1
+    if number_of_categories_to_drop == len(indices_of_all_categories):
+        number_of_categories_to_drop -= 1
 
-    dropped_guideline_indices = random.sample(
-        nonviolated_guideline_indices, number_of_guidelines_to_drop
+    dropped_category_indices = random.sample(
+        nonviolated_category_indices, number_of_categories_to_drop
     )
 
-    retained_guideline_indices = list(
-        set(indices_of_all_guidelines) - (set(dropped_guideline_indices))
+    retained_category_indices = list(
+        set(indices_of_all_categories) - (set(dropped_category_indices))
     )
 
     sft_rows_to_build.append(
-        make_sft_row(
-            eval_row,
+        create_formatted_finetuning_example(
+            training_example,
             args,
-            guideline_indeces_to_include_in_llama_guard_prompt=retained_guideline_indices,
+            category_indeces_to_include_in_llama_guard_prompt=retained_category_indices,
         )
     )
 
 
-def maybe_augment_with_row_with_dropped_violated_and_nonviolated_prompt_guidelines(
-    eval_row: EvalRow,
+def maybe_augment_with_row_with_dropped_violated_and_nonviolated_prompt_categories(
+    training_example: TrainingExample,
     sft_rows_to_build: list[dict[str, str]],
-    indices_of_all_guidelines: range,
-    violated_guideline_indices: list[int],
-    nonviolated_guideline_indices: list[int],
+    indices_of_all_categories: range,
+    violated_category_indices: list[int],
+    nonviolated_category_indices: list[int],
     args: DatagenArgs,
 ) -> None:
     if (
-        eval_row.label == "safe"
-        or not args.should_augment_with_rows_with_dropped_violated_and_nonviolated_prompt_guidelines
+        training_example.label == "safe"
+        or not args.should_augment_with_rows_with_dropped_violated_and_nonviolated_prompt_categories
     ):
         pass
 
-    random_nonviolated_guideline_indices_to_drop = random.sample(
-        nonviolated_guideline_indices,
-        random.randint(0, len(nonviolated_guideline_indices) - 1),
+    random_nonviolated_category_indices_to_drop = random.sample(
+        nonviolated_category_indices,
+        random.randint(0, len(nonviolated_category_indices) - 1),
     )
 
-    set_of_retained_guideline_indices = (
-        set(indices_of_all_guidelines)
-        - set(violated_guideline_indices)
-        - set(random_nonviolated_guideline_indices_to_drop)
+    set_of_retained_category_indices = (
+        set(indices_of_all_categories)
+        - set(violated_category_indices)
+        - set(random_nonviolated_category_indices_to_drop)
     )
 
-    eval_row_copy = copy.deepcopy(eval_row)
-    eval_row_copy.label = "safe"
-    eval_row_copy.violated_guideline_codes = []
+    training_example_copy = copy.deepcopy(training_example)
+    training_example_copy.label = "safe"
+    training_example_copy.violated_category_codes = []
 
     sft_rows_to_build.append(
-        make_sft_row(
-            eval_row_copy,
+        create_formatted_finetuning_example(
+            training_example_copy,
             args,
-            guideline_indeces_to_include_in_llama_guard_prompt=list(
-                set_of_retained_guideline_indices
+            category_indeces_to_include_in_llama_guard_prompt=list(
+                set_of_retained_category_indices
             ),
         )
     )
